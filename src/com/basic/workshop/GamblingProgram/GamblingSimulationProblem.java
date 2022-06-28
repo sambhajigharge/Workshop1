39b21da9244e3c76573276c8b0b8973815953d2d
sushant <sgharge299@gmail.com>
2022-06-28 15:21:48 +0530

    [sushant]worked on the resign condition of the Gambler when win or loose.

--- src/com/basic/workshop/GamblingProgram/GamblingSimulationProblem.java
+++ src/com/basic/workshop/GamblingProgram/GamblingSimulationProblem.java
@@ -4,22 +4,48 @@ import java.util.Scanner; // import scanner class
 
 public class GamblingSimulationProblem {
 
+    static int AMOUNT_OF_STAKE = 100;
+    static int BET_AMOUNT = 1;
+    static final int WON = 1;
+    static final int LOST = 0;
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in); // create scanner object
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Starting amount of stake is " + AMOUNT_OF_STAKE);
+        System.out.println("Amount to bet after every play " + BET_AMOUNT);
+        int cashInHand = AMOUNT_OF_STAKE;
 
-        System.out.println("Enter amount of stake :");
+        while (cashInHand > 50 && cashInHand < 150) {
+            int play = (int) Math.floor(Math.random() * 10) % 2;
+            System.out.println("Gamblers play:" + play);
 
-        int amount = sc.nextInt();
+            switch (play) {
+                case WON:
+                    cashInHand = cashInHand + BET_AMOUNT;
+                    System.out.println("Total current cash gambler has " + cashInHand);
+                    break;
+                case LOST:
+                    cashInHand = cashInHand - BET_AMOUNT;
+                    System.out.println("Total current cash gambler has " + cashInHand);
+                    break;
+                default:
+                    System.out.println("default");
+            }
+            if (play == 1) {
+                System.out.println("Player won the current play");
+            } else {
+                System.out.println("Player lost the current play");
+            }
 
-        System.out.println("Enter amount to bet after every play :");
+        }
+        if (cashInHand == 150 || cashInHand == 50) {
+            System.out.println("Player will resign for current day");
 
-        int bet = sc.nextInt();
-
-        System.out.println("Amount of stake is "+amount);
-        
-        System.out.println("Amount to bet after every game "+bet);
-
+        }
     }
 
 }
+
+
+
+        
