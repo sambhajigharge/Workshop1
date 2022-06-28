86bc5f51bfba7f958d7ebaf409e98a5005533183
sushant <sgharge299@gmail.com>
2022-06-28 13:16:04 +0530

    [Sushant]worked on the stake of every day and bet of every game

--- src/com/basic/workshop/GamblingProgram/GamblingSimulationProblem.java
+++ src/com/basic/workshop/GamblingProgram/GamblingSimulationProblem.java
@@ -1,7 +1,25 @@
 package com.basic.workshop;
 
+import java.util.Scanner; // import scanner class
+
 public class GamblingSimulationProblem {
-	public static void main(String [] args) {
-		System.out.println("Welcome to the Gambling Simulator Program.");
-	}
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); // create scanner object
+
+        System.out.println("Enter amount of stake :");
+
+        int amount = sc.nextInt();
+
+        System.out.println("Enter amount to bet after every play :");
+
+        int bet = sc.nextInt();
+
+        System.out.println("Amount of stake is "+amount);
+        
+        System.out.println("Amount to bet after every game "+bet);
+
+    }
+
 }
